d22152395e145e8f35d73a07616f6acd87cc6b8f
manish-gitx <108692670+manish-gitx@users.noreply.github.com>
2022-08-17 13:42:58 +0530

    .

---
+++ fundamentals/AddTwoNumbers.java
@@ -0,0 +1,11 @@
+package fundamentals;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int a=10;// int should he used to decalare the data type
+        int b=17;
+        int c=a+b;
+        System.out.println(c);
+    }
+    
+}
