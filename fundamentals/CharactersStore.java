6891da6d88782cbc7abcc7b118eddae47de0e58c
manish-gitx <108692670+manish-gitx@users.noreply.github.com>
2022-08-18 14:24:11 +0530

    Create CharactersStore.java

---
+++ fundamentals/CharactersStore.java
@@ -0,0 +1,18 @@
+package fundamentals;
+
+public class CharactersStore {
+    public static void main(String[] args) {
+        // int a=Integer.MAX_VALUE;
+        // int b=Integer.MIN_VALUE;
+        // System.out.println(a +" is max value "+b+" is min value");
+
+        int i='a';
+        System.out.println('a'+3);
+        System.out.println(i);
+        
+        
+
+    
+    }
+    
+}
