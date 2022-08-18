28aace55bff1ecc29dc3d9a2d3ecf2b1f94706e7
manish-gitx <108692670+manish-gitx@users.noreply.github.com>
2022-08-18 14:24:13 +0530

    Create TypeConversions.java

---
+++ fundamentals/TypeConversions.java
@@ -0,0 +1,20 @@
+package fundamentals;
+
+public class TypeConversions {
+    public static void main(String[] args) {
+        // char ch='c';
+        // int i =ch;
+        // ch=(char)(i+2);
+        // System.out.println(ch);
+        // int i=40;
+        // short s=(short)(i);
+        // System.out.println(s)
+        double deci=1.8;
+        float fl=(float)deci;
+        System.out.println(fl);
+
+
+    }
+
+    
+}
