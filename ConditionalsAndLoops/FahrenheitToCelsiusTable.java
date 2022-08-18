cdf5b4b16d7bc751e1883aee34e9fc52de66bccf
manish-gitx <108692670+manish-gitx@users.noreply.github.com>
2022-08-19 03:07:20 +0530

    Create FahrenheitToCelsiusTable.java

---
+++ ConditionalsAndLoops/FahrenheitToCelsiusTable.java
@@ -0,0 +1,22 @@
+package ConditionalsAndLoops;
+
+import java.util.Scanner;
+
+public class FahrenheitToCelsiusTable {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int start_value = in.nextInt();
+        int end_value=in.nextInt();
+        int gap_value=in.nextInt();
+        while(end_value>=start_value){
+            int c = (start_value -32)*5/9;
+            System.out.println(start_value+" "+c);
+            start_value=start_value+gap_value;
+
+
+        }
+        
+    }
+    
+    
+}
