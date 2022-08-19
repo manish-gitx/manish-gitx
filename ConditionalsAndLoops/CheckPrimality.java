8b2ad38b6d3cec5e61771520afd75937634ffc18
manish-gitx <108692670+manish-gitx@users.noreply.github.com>
2022-08-19 19:04:16 +0530

    Create CheckPrimality.java

---
+++ ConditionalsAndLoops/CheckPrimality.java
@@ -0,0 +1,22 @@
+package ConditionalsAndLoops;
+
+import java.util.Scanner;
+
+public class CheckPrimality {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        int n=in.nextInt();
+        int i =2;
+        while(i<n){
+            if(n%i==0){
+                System.out.println("composite");
+                in.close();
+                return;//return statement will return to main so ntg will be excuted
+            }
+        i=i+1;    
+        }
+    System.out.println("prime");
+    in.close();
+    }
+    
+}
