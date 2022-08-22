6599013836c8c7fa8d58adde4ca1075693a9ecd6
manish-gitx <108692670+manish-gitx@users.noreply.github.com>
2022-08-22 23:54:14 +0530

    Create InvertedNumberPatt.java

---
+++ Patterns_2/InvertedNumberPatt.java
@@ -0,0 +1,21 @@
+package Patterns_2;
+
+import java.util.Scanner;
+
+public class InvertedNumberPatt{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int i=1;
+        while(i<=n){
+            int j=1;
+            while(j<=n-i+1){
+                System.out.print(n-i+1);
+                j=j+1;
+            }
+            System.out.println();
+            i=i+1;
+        
+        }
+    }
+}
