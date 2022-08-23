c8a5eceb390bc7372ff76466ac8c9e84813513ae
manish-gitx <108692670+manish-gitx@users.noreply.github.com>
2022-08-23 21:29:06 +0530

    Update StarPattern.java

--- Patterns_2/StarPattern.java
+++ Patterns_2/StarPattern.java
@@ -27,6 +27,28 @@ public class StarPattern {
             System.out.println();
         }
         in.close();
+        /*import java.util.Scanner;
+public class Solution {
+public static void main(String[] args) {
+Scanner s = new Scanner(System.in);
+int n = s.nextInt();
+int currRow = 1;
+while(currRow <= n) {
+int spaces = 1;
+while(spaces <= (n - currRow)) {
+System.out.print(" ");
+spaces += 1;
+}
+int currCol = 1;
+while(currCol <= (2 * currRow) - 1) {
+System.out.print("*");
+currCol += 1;
+}
+System.out.println();
+currRow += 1;
+}
+}
+} */
 
         
     }
