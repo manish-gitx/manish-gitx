8d0f2f7831ce9e276ede01db499b72db468b8ec2
manish-gitx <108692670+manish-gitx@users.noreply.github.com>
2022-08-28 00:13:07 +0530

    Create patt4.java

---
+++ Patterns_1/Practice/patt4.java
@@ -0,0 +1,36 @@
+package Practice;
+import java.util.Scanner;
+public class patt4 {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int star=1;
+        for(int i=1;i<=n-1;i++){
+            int col=1;
+            for(;col<=n-i+1; col++){
+                System.out.print(col);
+            }
+            if(i>=2){
+                star*=2;
+                col=1;
+                for(;star>=col;col++){
+                    System.out.print("*");
+                }
+            }
+            int num=n-i+1;
+            for(;num>=1;num-=1){
+                System.out.print(num);
+            }
+            System.out.println();
+        }
+        in.close();
+
+/*
+12344321
+123​**​321
+12​****​21 */
+
+
+    }
+    
+}
