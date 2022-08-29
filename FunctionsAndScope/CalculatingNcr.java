4fc7e53d3033a7dd61202bb138483fb5d98d46df
manish-gitx <108692670+manish-gitx@users.noreply.github.com>
2022-08-30 00:08:45 +0530

    Update CalculatingNcr.java

--- FunctionsAndScope/CalculatingNcr.java
+++ FunctionsAndScope/CalculatingNcr.java
@@ -1,8 +1,25 @@
 package FunctionsAndScope;
 
+import java.util.Scanner;
+
 public class CalculatingNcr {
-    public static void main(String[] args) {
-        
+    public static int factorial(int num){
+        int fact=1;
+        for(int i=1;i<=num;i++){
+            fact=fact*i;
         }
-    
+        return fact;
+    }
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int r=in.nextInt();
+        int factn=factorial(n);
+        int factr=factorial(r);
+        int factnr=factorial(n-r);
+        int ans=factn/(factr*factnr);
+        System.out.println(ans);
+        in.close();
+
+    }
 }
