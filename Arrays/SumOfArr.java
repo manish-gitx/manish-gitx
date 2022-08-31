c6546aa0b9c938cc3e1402b6420a53bc0c8e82a1
manish-gitx <108692670+manish-gitx@users.noreply.github.com>
2022-08-31 19:23:19 +0530

    Update SumOfArr.java

--- Arrays/SumOfArr.java
+++ Arrays/SumOfArr.java
@@ -22,16 +22,10 @@ public class SumOfArr {
 	}
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int t=in.nextInt();
-        for(int i=1;i<=t;i++){
-            int arri[]=input();
-            int total=sum(arri);
-            System.out.println(total+"ans");
-            in.close();
-        }
-
-
-        
+        int arr[]=input();
+        int total=sum(arr);
+        System.out.println(total+"ans");
+        in.close();  
     }
 
     
