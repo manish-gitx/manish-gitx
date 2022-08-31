89074cc5fe5fc80eae055c9d57a6dda09efb3fca
manish-gitx <108692670+manish-gitx@users.noreply.github.com>
2022-08-31 17:17:29 +0530

    Create SumOfArr.java

---
+++ Arrays/SumOfArr.java
@@ -0,0 +1,37 @@
+package Arrays;
+import java.util.Scanner;
+public class SumOfArr {
+    public static int[] input(){
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=in.nextInt();
+        }
+        in.close();
+        return arr;
+    }
+    public static int sum(int[] arr) {
+        int total=0;
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            int sum=arr[i];
+            total=total+sum;
+        }
+        return total;
+	}
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=1;i<=t;i++){
+            int arri[]=input();
+            int total=sum(arri);
+            System.out.println(total+"ans");
+        }
+
+
+        
+    }
+
+    
+}
