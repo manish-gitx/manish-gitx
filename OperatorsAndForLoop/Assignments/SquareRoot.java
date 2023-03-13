61ddd0921208ac0e1fb9864090b6caacb5372abf
manish-gitx <108692670+manish-gitx@users.noreply.github.com>
2023-03-13 14:31:06 +0530

    Update SquareRoot.java

--- OperatorsAndForLoop/Assignments/SquareRoot.java
+++ OperatorsAndForLoop/Assignments/SquareRoot.java
@@ -4,22 +4,14 @@ import java.util.Scanner;
 
 public class SquareRoot {
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        int n=in.nextInt();
-        int power=0;
-        int i=1;
-        for(; power<=n; i++){
-            power=i*i;
+        try (Scanner in = new Scanner(System.in)) {
+            int n=in.nextInt();
+            int power=0;
+            for(;power*power<=n;){
+                power++;
+            }
+            System.out.println(power-1);
             in.close();
-            if(power==n){
-                System.out.println(i);
-                return;
-            }
-            else if(power>n){
-                System.out.println(i-1);
-                return;
-            }
         }
     }
-    
 }
